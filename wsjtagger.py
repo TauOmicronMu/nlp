33e3c4ae62ac4a065b735df1735d01a29da52390
tauomicronmu <tauomicronmurs@gmail.com>
2016-11-23 00:32:16 +0000

    loaded in the data ready to form a tagger

--- wsjtagger.py
+++ wsjtagger.py
@@ -1,3 +1,11 @@
 import nltk
+import nltk.data
 
-from nltk.tag.stanford import NERTagger
+FP = "wsj_training/concatstr.txt"
+
+# Load in the tagged data as a single string from the filepath.
+data = ""
+with open(FP, "r") as f:
+    data = f.read()
+
+
