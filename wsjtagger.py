d4ce363eb6f2bb10a1b2c87d9725d95134aaea91
tauomicronmu <tauomicronmurs@gmail.com>
2016-11-24 22:00:03 +0000

    OH MY WHAT... TAGGING KINDA WORKS!

--- wsjtagger.py
+++ wsjtagger.py
@@ -5,6 +5,7 @@ import re
 from collections import Counter
 
 FP = "wsj_training/concatstr.txt"
+UFP = "wsj_untagged/concatstr.txt"
 
 tag_map = {
     "ORGANIZATION": "ORG",
@@ -28,10 +29,6 @@ organizations = getAllOfType("ORGANIZATION")
 people = getAllOfType("PERSON")
 locations = getAllOfType("LOCATION")
 
-print(organizations.most_common(10))
-print(people.most_common(10))
-print(locations.most_common(10))
-
 # We can use this to tag new data in the following way:
 #
 # FOR WORD Wn in UNTAGGED_DATA
@@ -46,4 +43,32 @@ print(locations.most_common(10))
 #     ELSE
 #          Tag Wn with the only tag it is assigned to. 
 
+# Load in untagged data
+untagged_data = ""
+with open(UFP, "r") as f:
+    untagged_data = f.read()
 
+untagged_words = re.findall(r'\w+', untagged_data)
+
+def tag(text):
+    organizations_score = organizations[text]
+    people_score = people[text]
+    locations_score = locations[text]
+
+    maxs = max(organizations_score, people_score, locations_score)
+    
+    if maxs == 0:
+        return text # If it doesn't fit, return untagged text.
+
+    if maxs == organizations_score:
+        return '<ENAMEX TYPE="ORGANIZATION">' + text + '</ENAMEX>'
+    if maxs == people_score:
+        return '<ENAMEX TYPE="PERSON">' + text + '</ENAMEX>'
+    if maxs == locations_score:
+        return '<ENAMEX TYPE="LOCATION">' + text + '</ENAMEX>'
+
+tagged_input = ""
+for word in untagged_words:
+       tagged_input += tag(word) + " "
+
+print(tagged_input)
