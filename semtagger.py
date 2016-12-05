056ac1ccfa6100bc26e38ce0b70184fbf9cec70e
tauomicronmu <tauomicronmurs@gmail.com>
2016-12-05 20:32:36 +0000

    changed to re.split rather than doing a MULTILINE fullmatch

--- semtagger.py
+++ semtagger.py
@@ -1,15 +1,27 @@
 import re
 import nltk
+import itertools
 
 # TODO: Sort out this list comp <3 
 # filenames = ["]
 
+def intersperse(iterable, delimiter):
+    it = iter(iterable)
+    yield next(it)
+    for x in it:
+        yield delimiter
+        yield x
+
 data = ""
 split_data = []
+header = ""
+abstract_word = ""
 abstract = ""
 with open("s_untagged/364.txt", "r") as f:
     data = f.read()
     split_data = re.split("[Aa]bstract\:", data)
+    header = split_data[0]
+    abstract_word = split_data[1]
     abstract = split_data[2]
 
 print(abstract)
@@ -19,4 +31,5 @@ paragraphs = abstract.split('\n\n')
 
 # For each paragraph, tokenise them using the nltk.sent_tokenize() function.
 sentences = [nltk.sent_tokenize(s) for s in paragraphs][1:]
-print(sentences)
+
+
